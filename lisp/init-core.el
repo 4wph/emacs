16fd45690b3ce372995a61fef736346bcef2859e
4wph <gustavoaraujob1@gmail.com>
2020-12-25 11:03:04 -0300

    Added expand-region

--- lisp/init-core.el
+++ lisp/init-core.el
@@ -11,10 +11,15 @@
   (setq projectile-project-search-path '("/home/HDD/Documents/7CC"))
   :bind-keymap
   ("C-c p" . projectile-command-map)
-  :bind (("C-c f" . projectile-find-file)
-	 ("C-c o" . projectile-switch-project)
-	 ("C-c k" . projectile-kill-buffers))
+  :bind
+  (("C-c f" . projectile-find-file)
+   ("C-c o" . projectile-switch-project)
+   ("C-c k" . projectile-kill-buffers))
   :config
   (projectile-mode +1))
+
+(use-package expand-region
+  :bind
+  ("C-=" . er/expand-region))
 
 (provide 'init-core)
