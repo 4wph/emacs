a077aca5236d330836a8ae500df7023dc48e21c5
4wph <gustavoaraujob1@gmail.com>
2021-01-02 17:32:51 -0300

    Fixes to evil config

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -1,10 +1,13 @@
 (use-package evil
+  :init
+  (setq evil-want-keybinding nil
+	evil-want-integration t)
   :config
   (evil-mode 1))
 
 (use-package evil-collection
   :config
-  ((evil-collection-init)))
+  (evil-collection-init))
 
 (use-package evil-surround
   :config
@@ -12,7 +15,7 @@
 
 (use-package evil-commentary
   :config
-  (evil-commentary mode))
+  (evil-commentary-mode))
 
 (use-package god-mode)
 
@@ -20,5 +23,23 @@
   :config
   (evil-define-key 'normal global-map " " 'evil-execute-in-god-state)
   (evil-define-key 'god global-map [escape] 'evil-god-state-bail))
+
+; https://github.com/emacs-evil/evil-collection#faq
+(use-package general
+  :ensure t
+  :init
+  (setq general-override-states '(insert
+                                  emacs
+                                  hybrid
+                                  normal
+                                  visual
+                                  motion
+                                  operator
+                                  replace))
+  :config
+  (general-define-key
+   :states '(normal visual motion)
+   :keymaps 'override
+   "SPC" 'evil-execute-in-god-state))
 
 (provide 'init-evil)
