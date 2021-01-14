81fbdf0130f95b4a3adcc2a3c6538f90b21725f9
4wph <gustavoaraujob1@gmail.com>
2021-01-14 18:04:02 -0300

    Added more evil packages

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -1,13 +1,26 @@
+(setq evil-want-keybinding nil
+      evil-want-integration t); Needed for evil-Collection
+
+(use-package evil-leader
+  :config
+  (global-evil-leader-mode)
+  (evil-leader/set-key
+    "p" projectile-command-map
+    "a" 'org-agenda))
+
 (use-package evil
-  :init
-  (setq evil-want-keybinding nil
-	evil-want-integration t)
   :config
   (evil-mode 1))
 
 (use-package evil-collection
   :config
   (evil-collection-init))
+
+(use-package evil-org
+  :hook (org-mode . (lambda () evil-org-mode))
+  :config
+  (require 'evil-org-agenda)
+  (evil-org-agenda-set-keys))
 
 (use-package evil-surround
   :config
@@ -21,7 +34,13 @@
 
 (use-package evil-god-state
   :config
-  (evil-define-key 'normal global-map " " 'evil-execute-in-god-state)
+  (evil-define-key 'normal global-map (kbd "SPC") 'evil-execute-in-god-state)
   (evil-define-key 'god global-map [escape] 'evil-god-state-bail)); FIXME does not play well with projectile bindings
+
+(use-package lispyville
+  :hook
+  ((emacs-lisp-mode lisp-mode clojure-mode hy-mode) . lispyville-mode)
+  :config
+  (lispyville-set-key-theme '(operators commentary prettify additional-motions slurp/barf-cp)))
 
 (provide 'init-evil)
