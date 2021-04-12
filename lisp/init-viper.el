c0e7fcf4b73e65c71b41c7d1220f74765f0a8d03
4wph <gustavoaraujob1@gmail.com>
2021-04-12 18:47:13 -0300

    Updated viper settings

--- lisp/init-viper.el
+++ lisp/init-viper.el
@@ -1,7 +1,11 @@
 (setq viper-mode t
       viper-inhibit-startup-message 't
-      viper-expert-level '5)
+      viper-expert-level '5
+      viper-ex-style-motion nil
+      viper-auto-indent t)
 
 (require 'viper)
+
+(set-face-attribute 'viper-minibuffer-insert nil :background nil :foreground "white")
 
 (provide 'init-viper)
