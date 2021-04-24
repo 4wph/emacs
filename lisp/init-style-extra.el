f7bae568663b79eeef8f9b9d393bd47a53925db2
4wph <gustavoaraujob1@gmail.com>
2021-04-24 20:06:36 -0300

    Added neotree

--- lisp/init-style-extra.el
+++ lisp/init-style-extra.el
@@ -1,4 +1,4 @@
-(global-display-line-numbers-mode)
+(add-hook 'prog-mode-hook 'display-line-numbers-mode)
 (global-hl-line-mode)
 
 (ensure-package 'all-the-icons-ivy-rich)
@@ -13,5 +13,10 @@
 (ensure-package 'all-the-icons-ibuffer)
 (setq  all-the-icons-ibuffer-human-readable-size t)
 (all-the-icons-ibuffer-mode 1)
+
+(ensure-package 'neotree)
+(setq doom-themes-neotree-file-icons t)
+(doom-themes-neotree-config)
+(global-set-key [f8] 'neotree-projectile-action)
 
 (provide 'init-style-extra)
